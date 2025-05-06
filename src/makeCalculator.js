70696fd53e55d9f983f5f0bbac675ee0844c675f
Denis Kasatskiy <azartofgod@gmail.com>
2025-05-06 21:28:09 +0300

    Add Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,51 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const calculator = {
+    get result() {
+      return result;
+    },
+
+    operate(callBack, operator) {
+      callBack(operator);
+
+      return this;
+    },
+
+    add(operator) {
+      result += operator;
+
+      return this;
+    },
+
+    subtract(operator) {
+      result -= operator;
+
+      return this;
+    },
+
+    multiply(operator) {
+      result *= operator;
+
+      return this;
+    },
+
+    divide(operator) {
+      result /= operator;
+
+      return this;
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
